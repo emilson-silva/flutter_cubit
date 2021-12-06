768aa18b4b89d5eb5ab9b77b017cab8a6212d7b6
Emilson Pereira da Silva <emilson.silva@trinusco.com.br>
2021-12-06 17:25:33 -0300

    Ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cubit/pages/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: WelcomePage(),
     );
   }
 }
