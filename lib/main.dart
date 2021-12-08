e8dac3ae4cdcaaee54268cadb696fff528534df3
Emilson Pereira da Silva <emilson.silva@trinusco.com.br>
2021-12-08 00:06:45 -0300

    Ajustes e creates pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cubit/pages/navpages/main_pages.dart';
 import 'package:flutter_cubit/pages/welcome_page.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: WelcomePage(),
+      home: MainPage(),
     );
   }
 }
