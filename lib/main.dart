d136a4138cea056052b068696bef4367541d8a05
Emilson Pereira da Silva <emilson.silva@trinusco.com.br>
2021-12-15 17:57:54 -0300

    Ajustes BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_cubit/pages/detail_page.dart';
-import 'package:flutter_cubit/pages/navpages/main_page.dart';
-import 'package:flutter_cubit/pages/welcome_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cubit/cubit/app_cubits.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +19,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DetailPage(),
+      home: BlocProvider<AppCubits>(
+        create:(context)=> AppCubits(),
+        child: ,
+      )
     );
   }
 }
