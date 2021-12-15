6384c43b7dc065bdf86a91f34211491fb519b40c
Emilson Pereira da Silva <emilson.silva@trinusco.com.br>
2021-12-15 14:00:56 -0300

    Ajustes appbuttons

--- lib/widgets/appbuttons.dart
+++ lib/widgets/appbuttons.dart
@@ -2,18 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cubit/constants/colors.dart';
 
 class AppButtons extends StatefulWidget {
-  const AppButtons({Key? key}) : super(key: key);
+  final Color color;
+  final Color backgroundColor;
+  double size;
+  final Color borderColor;
+   AppButtons({
+    Key? key,
+    required this.color,
+    required this.backgroundColor,
+    required this.borderColor,
+    required this.size,
+  }) : super(key: key);
 
   @override
   _AppButtonsState createState() => _AppButtonsState();
 }
-
 class _AppButtonsState extends State<AppButtons> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 60,
-      height: 60,
+      width: size,
+      height: size,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
         color: AppColors.buttonBackground,
