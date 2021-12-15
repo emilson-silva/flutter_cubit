fb0b3e0aa9ca441de8a61044280dcfe8be7a2da3
Emilson Pereira da Silva <emilson.silva@trinusco.com.br>
2021-12-15 17:59:11 -0300

    Ajustes

---
+++ lib/cubit/app_cubits.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cubit/cubit/app_cubit_states.dart';
+
+class AppCubits extends Cubit<CubitStates>{
+  AppCubits() : super(InitialState()){
+    emit(WelcomeState());
+  }
+  
+}
