68f3cdc4d4c0d2918145ca63051e0a43169be481
Emilson Pereira da Silva <emilson.silva@trinusco.com.br>
2021-12-17 09:20:33 -0300

    data model

---
+++ lib/model/data_model.dart
@@ -0,0 +1,19 @@
+class  DataModel{
+  String name;
+  String img;
+  int price;
+  int people;
+  int stars;
+  String description;
+
+  DataModel({
+    required this.name,
+    required this.img,
+    required this.price,
+    required this.people,
+    required this.description,
+    required this.stars,
+  })
+  
+
+}
